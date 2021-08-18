0501e2005480e8736d992a7aa1ea60c1bccd1e0b
Tim Hosgood <timhosgood@gmail.com>
2021-08-18 02:20:55 +0100

    testing _output.yml

--- main_build.sh
+++ main_build.sh
@@ -6,5 +6,8 @@ mkdir -p build
 cp README.md build/index.md
 cp -R assets build/
 
+cp _output.yml SGA-1/
+cp _output.yml SGA-6/
+
 cd SGA-1 && ./build.R && mv build ../build/SGA-1 && cd -
 cd SGA-6 && ./build.R && mv build ../build/SGA-6 && cd -
