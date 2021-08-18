e17c6b158cf0ce608390f0de178434ad1f4dd423
Tim Hosgood <timhosgood@gmail.com>
2021-08-18 01:48:16 +0100

    assets

--- main_build.sh
+++ main_build.sh
@@ -2,6 +2,7 @@
 
 mkdir -p build
 rm -r build/*
+cp -R assets build/assets
 cd SGA-1 && ./build.R && mv build ../build/SGA-1 && cd -
 cd SGA-6 && ./build.R && mv build ../build/SGA-6 && cd -
 cp README.md build/index.md
