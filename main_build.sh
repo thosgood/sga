9e23e5d9db024588ad343597e09a95e9318462e8
Tim Hosgood <timhosgood@gmail.com>
2021-08-18 02:00:44 +0100

    .....

--- main_build.sh
+++ main_build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+[ -d "build" ] && rm -r build
 mkdir -p build
-rm -r build/*
-cp -R assets build/assets
+
+cp README.md build/index.md
+cp -R assets build/
+
 cd SGA-1 && ./build.R && mv build ../build/SGA-1 && cd -
 cd SGA-6 && ./build.R && mv build ../build/SGA-6 && cd -
-cp README.md build/index.md
