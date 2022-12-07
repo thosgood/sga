786c1094550b5a1a5521cbee7b48178a828d1657
Tim Hosgood <timhosgood@gmail.com>
2022-12-07 02:08:20 +0100

    ah github won't forget case

--- SGA-6/build.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-
-bookdown::render_book("index.Rmd",
-                      "bookdown::gitbook",
-                      output_dir = "build",
-                      config_file = "_bookdown.yml")
-print('SGA 6 done!')
